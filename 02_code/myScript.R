01f543fa31bda2191ce895c3e5907e118f3496b0
Oliver Guggenbühl <oliver.guggenbuehl@statworx.com>
2019-02-06 14:01:41 +0100

    added project & folders

--- 02_code/myScript.R
+++ 02_code/myScript.R
@@ -1,3 +1,28 @@
 library(readr)
+library(dplyr)
+library(ggplot2)
+library(forcats)
 
 df <- read_csv("01_data/us-500.csv")
+
+plot_data <- df %>%
+  group_by(state) %>%
+  count()
+
+write_csv(plot_data, "03_output/plot_data.csv")
+
+plot <- plot_data %>% 
+  ggplot()+
+  geom_col(aes(fct_reorder(state, n), 
+               n, 
+               fill = n))+
+  coord_flip()+
+  labs(
+    title = "Number of people by state",
+    subtitle = "From US-500 dataset",
+    x = "State",
+    y = "Number of people"
+  )+ 
+  theme_bw()
+
+ggsave("03_output/myplot.png", width = 10, height = 8, dpi = 100)
