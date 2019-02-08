270659d9eb3246ff48783e5fa546e9b2e8fd6399
Oliver Guggenbühl <oliver.guggenbuehl@statworx.com>
2019-02-08 11:35:07 +0100

    finalized files

--- 02_code/myScript.R
+++ 02_code/myScript.R
@@ -3,14 +3,18 @@ library(dplyr)
 library(ggplot2)
 library(forcats)
 
+# import dataframe
 df <- read_csv("01_data/us-500.csv")
 
+# manipulate data
 plot_data <- df %>%
   group_by(state) %>%
   count()
 
+# save manipulated data to output folder
 write_csv(plot_data, "03_output/plot_data.csv")
 
+# create plot based on manipulated data
 plot <- plot_data %>% 
   ggplot()+
   geom_col(aes(fct_reorder(state, n), 
@@ -25,4 +29,5 @@ plot <- plot_data %>%
   )+ 
   theme_bw()
 
+# save plot to output folder
 ggsave("03_output/myplot.png", width = 10, height = 8, dpi = 100)
--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # Base image https://hub.docker.com/u/rocker/
-FROM oliverstatworx/base-r-tidyverse:latest
+FROM rocker/r-base:latest
 
 ## create directories
 RUN mkdir -p /01_data
@@ -7,10 +7,8 @@ RUN mkdir -p /02_code
 RUN mkdir -p /03_output
 
 ## copy files
-COPY ./02_code /02_code
+COPY /02_code/install_packages.R /02_code/install_packages.R
+COPY /02_code/myScript.R /02_code/myScript.R
 
 ## install R-packages
-# RUN Rscript /setup/install_packages.R
-
-## run the script
-# CMD Rscript /02_code/myScript.R
+RUN Rscript /02_code/install_packages.R
